94bff1cb97eea5a3e68b948ed97281e88830cfc5
Arseniy Kanev <arseniy.kan@gmail.com>
2017-03-24 17:00:31 +0300

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
-User.create(email: 'user@mail.ru', password: 'password', password_confirmation: 'password')
+user = User.create(email: 'user@mail.ru', password: 'password', password_confirmation: 'password')
 User.create(email: 'admin@mail.ru', password: 'password', password_confirmation: 'password', role: "admin")
+
+10.times do
+  Task.create(name: Faker::Lorem.word, description: Faker::Lorem.paragraph, user: user)
+end
